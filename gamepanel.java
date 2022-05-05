494b399ab88e09e8c50418b41a544543bba9e887
nukman4 <101593515+nukman4@users.noreply.github.com>
2022-05-05 12:39:35 -0600

    Update gamepanel.java

--- gamepanel.java
+++ gamepanel.java
@@ -3,16 +3,26 @@ import java.awt.Dimension;
 import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
+import java.awt.*;
+import java.io.*;
+import javax.imageio.ImageIO;
 public class gamepanel extends JPanel implements Runnable{
-  //screen settings
+  //variables
+  Image player;
+  Image playerfront;
   final int screenwidth = 400;
   final int screenheight = 400;
-  //fps
-  int FPS = 60;
-  
   KeyHandler keyH = new KeyHandler();
   Thread gameThread;
-
+  void loadimg(){
+    try{
+    playerfront = ImageIO.read(new File("sprites/playerfront.png"));
+    } catch(Exception e){
+      System.out.println(e);
+    };
+  }
+  
+  //game panel set up
   public gamepanel(){
     this.setPreferredSize(new Dimension(screenwidth,screenheight));
     this.setBackground(Color.black);
@@ -21,39 +31,46 @@ public class gamepanel extends JPanel implements Runnable{
     this.setFocusable(true);
   }
 
+  
+  //gameloop stuff
   public void startGameThread(){
     gameThread = new Thread(this);
     gameThread.start();
+    loadimg();
   }
   @Override
   public void run(){
     while(gameThread != null){
         update();
         repaint();
-        try{Thread.sleep(1000/FPS);}catch(Exception e){System.out.println(e);}
+        try{Thread.sleep(1000/60);}catch(Exception e){System.out.println(e);}
       }
   }
-  int x=100;
-  int y=100;
+
+  
+  //update stuff
   public void update(){
     if(keyH.uppress == true){
-      y-=3;
+      player.y-=3;
     }
     if(keyH.downpress == true){
-      y+=3;
+      player.y+=3;
     }
     if(keyH.leftpress == true){
-      x-=3;
+      player.x-=3;
     }
     if(keyH.rightpress == true){
-      x+=3;
+      player.x+=3;
     }
   }
+
+  
+  //what to paint
   public void paintComponent(Graphics g){
     super.paintComponent(g);
     Graphics2D g2 = (Graphics2D)g;
     g2.setColor(Color.white);
-    g2.fillRect(x,y,20,20);
+    g2.drawImage(playerfront, player.x, player.y, null);
     g2.dispose();
   }
 }
